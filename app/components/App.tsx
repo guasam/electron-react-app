72a2d13013ff2ca5b1958ef3d27aef0f4207a038
guasam <guasamos@outlook.com>
2025-05-31 14:37:24 +0530

    Prototype: initialize shadcn (wip)

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,6 +1,37 @@
 import WelcomeKit from '@/lib/welcome/WelcomeKit'
+import { Badge } from './ui/badge'
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from './ui/select'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
+import { Button } from './ui/button'
 import '../styles/app.css'
 
 export default function App() {
-  return <WelcomeKit />
+  return (
+    <div>
+      <span className="text-red-500">This is good</span>
+      <br />
+      <Button variant="secondary">This is good</Button>
+      <br />
+      <Badge>Crazy four</Badge>
+      <br />
+
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <br />
+
+      <HoverCard>
+        <HoverCardTrigger>Hover</HoverCardTrigger>
+        <HoverCardContent>The React Framework – created and maintained by @vercel.</HoverCardContent>
+      </HoverCard>
+    </div>
+  )
 }
--- app/renderer.tsx
+++ app/renderer.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
 import appIcon from '@/resources/build/icon.png'
 import { WindowContextProvider, menuItems } from '@/lib/window'
+import App from './components/app'
 import '@/lib/window/window.css'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
